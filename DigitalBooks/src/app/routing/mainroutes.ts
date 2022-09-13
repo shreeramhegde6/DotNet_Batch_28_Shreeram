7d5f6470e62f55c9d9b440f292e97921b6b2c88e
unknown <cogdotnet1708@iiht.tech>
2022-09-13 11:15:04 +0530

    navbar and Login

--- DigitalBooks/src/app/routing/mainroutes.ts
+++ DigitalBooks/src/app/routing/mainroutes.ts
@@ -2,8 +2,9 @@ import { HomeComponent } from "../home/home.component";
 import { SignupComponent } from "../signup/signup.component";
 
 export const Mainroutes=[
-    {path:'home',component : HomeComponent},
+    {path:'',component : HomeComponent},
     {path : 'signup', component : SignupComponent},
+    {path:'home',component : HomeComponent},
     {path : 'signupadd', loadChildren :()=>import('../signup/signup.module').then(m=>m.SignupModule)}
     
 ]
